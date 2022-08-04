c29c4a4eeacc396bc2a1e4cba0b501ce2aff434e
Krystian Hetmanek <tquboid@gmail.com>
2022-08-04 17:50:14 +0200

    add loop for operations on numbers

--- Zadanie_Kalkulator.py
+++ Zadanie_Kalkulator.py
@@ -11,10 +11,35 @@ def mnozenie(a, b):
     return a * b
 
 def dzielenie(a, b):
-    if a == 0 :
-        print('Nie można dzielić przez 0!')
-        quit()
-    else:
-        return a / b
+    return a / b
     
+print('Wybierz działanie: 1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie')
+while True:
+    n = int(input())
 
+    if n == 0:
+        print('Dziękujemy za kożystanie z kalkulatora. Nie zapraszamy ponownie.')
+        break
+    if n == 1:
+        print('Podaj 2 liczby:')
+        x = int(input())
+        y = int(input())
+            #dodac logging
+    if n == 2:
+        print("Podaj 2 liczby:")
+        x = int(input())
+        y = int(input())
+            #dodac logging
+    if n == 3:
+        print('Podaj 2 liczby:')
+        x = int(input())
+        y = int(input())
+            #dodac logging
+    if n == 4:
+        print('Podaj 2 liczby:')
+        x = int(input())
+        y = int(input())
+            #dodac logging
+    else:
+        print('Proszę podać liczbę z zakresu 0-4.')
+        break
